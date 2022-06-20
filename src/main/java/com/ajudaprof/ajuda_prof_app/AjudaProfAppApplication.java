9e29ec3835e9a1224d6c0ac64d5b3e472518266e
Guilherme Daniel <1181743@isep.ipp.pt>
2022-06-20 11:51:59 +0100

    Criaão

---
+++ src/main/java/com/ajudaprof/ajuda_prof_app/AjudaProfAppApplication.java
@@ -0,0 +1,13 @@
+package com.ajudaprof.ajuda_prof_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjudaProfAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AjudaProfAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ajudaprof/ajuda_prof_app/AjudaProfAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajudaprof.ajuda_prof_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjudaProfAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
